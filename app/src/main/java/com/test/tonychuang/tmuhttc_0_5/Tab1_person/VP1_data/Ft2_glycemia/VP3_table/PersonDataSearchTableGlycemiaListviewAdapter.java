67bc00ce45d5f0b8305ffe049d82d45d0e91fd66
TonyChuang <aaa_bbb_0088@hotmail.com>
2016-04-14 12:17:09 +0800

    修正DRAW無資料問題

---
+++ app/src/main/java/com/test/tonychuang/tmuhttc_0_5/Tab1_person/VP1_data/Ft2_glycemia/VP3_table/PersonDataSearchTableGlycemiaListviewAdapter.java
@@ -0,0 +1,189 @@
+package com.test.tonychuang.tmuhttc_0_5.Tab1_person.VP1_data.Ft2_glycemia.VP3_table;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.test.tonychuang.tmuhttc_0_5.R;
+import com.test.tonychuang.tmuhttc_0_5.Z_other.MyDataModule.MyDateSFormat;
+import com.test.tonychuang.tmuhttc_0_5.Z_other.SQLiteDB.RowDataFormat.GlyDataRow;
+import com.test.tonychuang.tmuhttc_0_5.Z_other.ShrPref.WLevelShrPref;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * Created by TonyChuang on 2016/4/6.
+ */
+public class PersonDataSearchTableGlycemiaListviewAdapter extends BaseAdapter {
+    private int BG_BM_Max;
+    private int BG_BM_Min;
+    private int BG_AM_Max;
+    private int BG_AM_Min;
+
+    private LayoutInflater myInflater;
+    private ArrayList<GlyDataRow> Table = null;
+    private String dataFlag;
+
+    public PersonDataSearchTableGlycemiaListviewAdapter(Context ctxt, ArrayList<GlyDataRow> Table, String dataFlag) {
+        WLevelShrPref wLevelShrPref = new WLevelShrPref(ctxt);
+        BG_BM_Max = wLevelShrPref.getBG_BM_Max();
+        BG_BM_Min = wLevelShrPref.getBG_BM_Min();
+        BG_AM_Max = wLevelShrPref.getBG_AM_Max();
+        BG_AM_Min = wLevelShrPref.getBG_AM_Min();
+
+        myInflater = LayoutInflater.from(ctxt);
+        this.dataFlag = dataFlag;
+        this.Table = Table;
+
+//        ArrayList<GlyDataRow> tmp = new ArrayList<>();
+//        tmp.addAll(Table);
+//        switch (dataFlag) {
+//            case "allBtn":
+//                break;
+//            case "bfBtn":
+//                for (int i = 0; i < tmp.size(); i++) {
+//                    if (tmp.get(i).getGData_meal_flag().equals("af")) {
+//                        tmp.remove(i);
+//                    }
+//                }
+//                break;
+//            case "afBtn":
+//                for (int i = 0; i < tmp.size(); i++) {
+//                    if (!tmp.get(i).getGData_meal_flag().equals("af")) {
+//                        tmp.remove(i);
+//                    }
+//                }
+//                break;
+//            default:
+//                break;
+//        }
+//        this.Table = tmp;
+    }
+
+    @Override
+    public int getCount() {
+        return Table.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return Table.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewTag viewTag;
+        if (convertView == null) {
+            //取得listItem容器 view
+            convertView = myInflater.inflate(R.layout.listview_item_glycemia_data, null);
+
+            //建構listItem內容view
+            viewTag = new ViewTag(
+                    (LinearLayout) convertView.findViewById(R.id.itemLayout),
+                    (TextView) convertView.findViewById(R.id.dataDate),
+                    (TextView) convertView.findViewById(R.id.mealFlag),
+                    (TextView) convertView.findViewById(R.id.gDataValue),
+                    (TextView) convertView.findViewById(R.id.memo)
+            );
+
+            convertView.setTag(viewTag);
+        } else {
+            viewTag = (ViewTag) convertView.getTag();
+        }
+
+        MyDateSFormat myDateSFormat = new MyDateSFormat();
+        SimpleDateFormat DateFormat = new SimpleDateFormat("M月d日\na h:mm", Locale.getDefault());
+        Date date = null;
+        try {
+            date = myDateSFormat.getFrmt_yMdHm().parse(Table.get(position).getGData_datetime());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        viewTag.dataDate.setText(DateFormat.format(date));
+        viewTag.dataDate.setTextSize(14f);
+        if (Table.get(position).getGData_meal_flag().equals("af")) {
+            viewTag.mealFlag.setText("飯後");
+            if (Table.get(position).getGData_value() > BG_AM_Max) {
+                viewTag.gDataValue.setTextColor(Color.RED);
+            } else if (Table.get(position).getGData_value() < BG_AM_Min) {
+                viewTag.gDataValue.setTextColor(Color.BLUE);
+            } else {
+                viewTag.gDataValue.setTextColor(Color.BLACK);
+            }
+        } else {
+            viewTag.mealFlag.setText("飯前");
+            if (Table.get(position).getGData_value() > BG_BM_Max) {
+                viewTag.gDataValue.setTextColor(Color.RED);
+            } else if (Table.get(position).getGData_value() < BG_BM_Min) {
+                viewTag.gDataValue.setTextColor(Color.BLUE);
+            } else {
+                viewTag.gDataValue.setTextColor(Color.BLACK);
+            }
+        }
+        viewTag.mealFlag.setTextSize(18f);
+        viewTag.gDataValue.setText(String.valueOf(Table.get(position).getGData_value()));
+        viewTag.gDataValue.setTextSize(18f);
+        viewTag.memo.setText("");
+        viewTag.memo.setTextSize(15f);
+
+        viewTag.itemLayout.setVisibility(View.VISIBLE);
+        switch (dataFlag) {
+            case "allBtn":
+                break;
+            case "bfBtn":
+                if (this.Table.get(position).getGData_meal_flag().equals("af")) {
+                    viewTag.itemLayout.setVisibility(View.GONE);
+                }
+                break;
+            case "afBtn":
+                if (!this.Table.get(position).getGData_meal_flag().equals("af")) {
+                    viewTag.itemLayout.setVisibility(View.GONE);
+                }
+                break;
+            default:
+                break;
+        }
+
+
+        /*以下为新增部分*/ //調整listview item寬度
+        AbsListView.LayoutParams lp = new AbsListView.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, 80);
+        convertView.setLayoutParams(lp);
+        /*以上为新增部分*/
+
+        return convertView;
+    }
+
+    class ViewTag {
+        LinearLayout itemLayout;
+        TextView dataDate;
+        TextView mealFlag;
+        TextView gDataValue;
+        TextView memo;
+
+        public ViewTag(LinearLayout itemLayout, TextView dataDate, TextView mealFlag, TextView gDataValue, TextView memo) {
+            this.itemLayout = itemLayout;
+            this.dataDate = dataDate;
+            this.mealFlag = mealFlag;
+            this.gDataValue = gDataValue;
+            this.memo = memo;
+        }
+    }
+}
